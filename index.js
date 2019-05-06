2cb1775b1f8f295ccadf7103b248e54aa25446b7
xulong <xulong@xulongdeMacBook-Pro-2.local>
2019-05-06 15:54:30 +0800

    fix: 初始化

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  config: require('./lib/config'),
+  Auth: require('./lib/Auth'),
+  Api: require('./lib/Api'),
+  WebHook: require('./lib/WebHook')
+};
---
+++ test/webhook.test.js
@@ -0,0 +1,35 @@
+/**
+ * Created by obzerg on 16/1/5.
+ */
+var should = require('should');
+var Yhsd = require('../index');
+
+describe('test/webhook.test.js', function () {
+    var webHook;
+    before(function () {
+        webHook = new Yhsd.WebHook('fbc7f83524f14e358a325a5066acd741');
+    });
+    it('get webhook instance should be fail', function () {
+        (function () {
+            new Yhsd.WebHook()
+        }).should.throw('缺少参数');
+    });
+
+    it('verifyHmac should be throw error', function () {
+        (function () {
+            webHook.verifyHmac()
+        }).should.throw('缺少参数');
+    });
+
+    it('verifyHmac should be fail', function () {
+        webHook.verifyHmac('123', '123').should.be.false();
+    });
+
+    it('should verify hmac as correct', function () {
+        webHook.verifyHmac('Fp6SL4UABMWhXojvSGBneRs0h0wEnqxQrqT/ko/mqg0=', '12345qwert').should.be.true();
+    });
+
+    it('should verify hmac in Chinese as correct', function () {
+        webHook.verifyHmac('FXX5JR4sC7Z6kktz/vhxLcy3ydEdIHWpAHnqOxnXhJU=', '12345qwert友好速搭很棒').should.be.true();
+    })
+});
